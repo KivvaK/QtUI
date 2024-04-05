596efd5f0c91408d0390781738b9209a94e30460
Seraph <k.kivva@gmail.com>
2024-04-05 21:30:41 +0300

    Добавил вывод текста на изображение в шпаргалку и в Painting

--- Painting/mainwindow.cpp
+++ Painting/mainwindow.cpp
@@ -13,7 +13,7 @@ void clear(QPixmap& image, const QColor color = QColor::fromRgb(0, 255, 255)) {
 }
 
 // Функция рисования треугольника на изображении image пером pn
-void draw(QPixmap& image, QPen& pn) {
+void draw(QPixmap& image, const QPen& pn) {
     // Создаём QPainter для изображения image
     QPainter p(&image);
     // Задаём перо для рисования
@@ -23,6 +23,25 @@ void draw(QPixmap& image, QPen& pn) {
     p.drawLine(100, 100, 200, 100);
     p.drawLine(200, 100, 150, 200);
     p.drawLine(150, 200, 100, 100);
+
+    // При выходе из функции p уничтожается, и все изменения сохраняются в image
+}
+
+// Функция вывода текста text в положение (x, y)
+// на изображении image пером pn и шрифтом fnt
+void print(const QString& text,
+           int x, int y,
+           QPixmap& image,
+           const QPen& pn,
+           const QFont& fnt) {
+    // Создаём QPainter для изображения image
+    QPainter p(&image);
+    // Задаём перо и шрифт для вывода текста
+    p.setPen(pn);
+    p.setFont(fnt);
+
+    // Выводим текст на изображение
+    p.drawText(x, y, text);
 
     // При выходе из функции p уничтожается, и все изменения сохраняются в image
 }
@@ -62,8 +81,16 @@ void MainWindow::on_pbDraw_clicked()
     image = QPixmap(ui->gvDrawer->rect().size());
     // Вызываем функцию очистки этого изображения (см. выше) цветом фона
     clear(image, bgColor);
+
     // Рисуем треугольник (см. выше) при помощи пера
     draw(image, pn);
+
+    // Создаём шрифт семейства Helvetica, размер 16, курсивный
+    QFont fnt("Helvetica", 16);
+    fnt.setItalic(true);
+    // Выводим на изображение текст (в нижний угол нарисованного треугольника)
+    print("Hello, world!", 150, 200, image, pn, fnt);
+
     // Меняем изображение в отображаемом объекте сцены на новое
     pmi->setPixmap(image);
 }
